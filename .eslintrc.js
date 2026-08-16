111533af9f1c405cdc863d3ed49f72cb1c4f401c
rahulra21900-netizen <rahulra21900@gmail.com>
2026-08-17 01:04:32 +0530

    fix(lint): silence unsupported typescript version warning in parserOptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
     node: true,
     es2022: true,
   },
+  parserOptions: {
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
